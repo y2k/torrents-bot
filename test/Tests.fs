da851d75d8851e08a1c76dbddcd996556667d617
y2k <itwisterlx@gmail.com>
2022-07-23 20:23:52 +0400

    fix tests

--- test/Tests.fs
+++ test/Tests.fs
@@ -21,16 +21,16 @@ let ``test my_rss_link`` () =
 let ``test search`` () =
     let expected =
         """Search result:
-ONE, Murata Yusuke / Ван, Мурата Юсукэ - Ванпанчме… <a href="https://t.me/$ME$?start=c2l0ZS5jb20vZm9ydW0vZGwucGhwP3Q9NTQwNDk5Ng==">[DOWNLOAD]</a>
-One Punch Man / Ванпанчмен [Art] [2020] [JPG] <a href="https://t.me/$ME$?start=c2l0ZS5jb20vZm9ydW0vZGwucGhwP3Q9NTkzNDk5Mw==">[DOWNLOAD]</a>
-One-Punch Man: A Hero Nobody Knows - Deluxe Editio… <a href="https://t.me/$ME$?start=c2l0ZS5jb20vZm9ydW0vZGwucGhwP3Q9NTg1NzY4MQ==">[DOWNLOAD]</a>
-(German Hip-Hop) MC Bomber - Official Discography… <a href="https://t.me/$ME$?start=c2l0ZS5jb20vZm9ydW0vZGwucGhwP3Q9NTUxNDAwNg==">[DOWNLOAD]</a>
-Ванпанчмен (ТВ-2) / One Punch Man 2 [TV] [1-4 из 1… <a href="https://t.me/$ME$?start=c2l0ZS5jb20vZm9ydW0vZGwucGhwP3Q9NTg3MzE2OA==">[DOWNLOAD]</a>
-One Finger Death Punch 2 [L] [RUS + ENG / ENG] (20… <a href="https://t.me/$ME$?start=c2l0ZS5jb20vZm9ydW0vZGwucGhwP3Q9NTcxOTE1OQ==">[DOWNLOAD]</a>
-[Nintendo Switch] One Finger Death Punch 2 [NSZ][E… <a href="https://t.me/$ME$?start=c2l0ZS5jb20vZm9ydW0vZGwucGhwP3Q9NTg2MjEyOQ==">[DOWNLOAD]</a>
-(Ongoing) One, Murata Yuusuke / Ван, Мурата Июсуке… <a href="https://t.me/$ME$?start=c2l0ZS5jb20vZm9ydW0vZGwucGhwP3Q9NDk1NTUxNw==">[DOWNLOAD]</a>
-Человек-тапок / One-Punch Man / Серии: 01-04 [2019… <a href="https://t.me/$ME$?start=c2l0ZS5jb20vZm9ydW0vZGwucGhwP3Q9NTgwMTQ4NA==">[DOWNLOAD]</a>
-Ванпанчмен / One-Punch Man (ТВ-2) (Сакурай Тикара)… <a href="https://t.me/$ME$?start=c2l0ZS5jb20vZm9ydW0vZGwucGhwP3Q9NTc2ODYwOA==">[DOWNLOAD]</a>"""
+- ONE, Murata Yusuke / Ван, Мурата Юсукэ - Ванпанчме… <a href="https://t.me/$ME$?start=c2l0ZS5jb20vZm9ydW0vZGwucGhwP3Q9NTQwNDk5Ng==">[ADD]</a>
+- One Punch Man / Ванпанчмен [Art] [2020] [JPG] <a href="https://t.me/$ME$?start=c2l0ZS5jb20vZm9ydW0vZGwucGhwP3Q9NTkzNDk5Mw==">[ADD]</a>
+- One-Punch Man: A Hero Nobody Knows - Deluxe Editio… <a href="https://t.me/$ME$?start=c2l0ZS5jb20vZm9ydW0vZGwucGhwP3Q9NTg1NzY4MQ==">[ADD]</a>
+- (German Hip-Hop) MC Bomber - Official Discography… <a href="https://t.me/$ME$?start=c2l0ZS5jb20vZm9ydW0vZGwucGhwP3Q9NTUxNDAwNg==">[ADD]</a>
+- Ванпанчмен (ТВ-2) / One Punch Man 2 [TV] [1-4 из 1… <a href="https://t.me/$ME$?start=c2l0ZS5jb20vZm9ydW0vZGwucGhwP3Q9NTg3MzE2OA==">[ADD]</a>
+- One Finger Death Punch 2 [L] [RUS + ENG / ENG] (20… <a href="https://t.me/$ME$?start=c2l0ZS5jb20vZm9ydW0vZGwucGhwP3Q9NTcxOTE1OQ==">[ADD]</a>
+- [Nintendo Switch] One Finger Death Punch 2 [NSZ][E… <a href="https://t.me/$ME$?start=c2l0ZS5jb20vZm9ydW0vZGwucGhwP3Q9NTg2MjEyOQ==">[ADD]</a>
+- (Ongoing) One, Murata Yuusuke / Ван, Мурата Июсуке… <a href="https://t.me/$ME$?start=c2l0ZS5jb20vZm9ydW0vZGwucGhwP3Q9NDk1NTUxNw==">[ADD]</a>
+- Человек-тапок / One-Punch Man / Серии: 01-04 [2019… <a href="https://t.me/$ME$?start=c2l0ZS5jb20vZm9ydW0vZGwucGhwP3Q9NTgwMTQ4NA==">[ADD]</a>
+- Ванпанчмен / One-Punch Man (ТВ-2) (Сакурай Тикара)… <a href="https://t.me/$ME$?start=c2l0ZS5jb20vZm9ydW0vZGwucGhwP3Q9NTc2ODYwOA==">[ADD]</a>"""
 
     let app = runTestApplication ()
     app.writeToBot "/search site.com one punch"
